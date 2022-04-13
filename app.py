0dc68f7d77fdb1dac0786cd83d7d1acf1689c375
Vince LaVecchia <lavec0324@msn.com>
2022-04-12 20:55:27 -0400

    Update challenge 2

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import scraping
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app2"
 mongo = PyMongo(app)
 
 @app.route("/")
--- scraping_old.py
+++ scraping_old.py
@@ -19,7 +19,6 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "hemisphere_data": hemisphere_data(browser),
         "last_modified": dt.datetime.now()
         }
 
@@ -94,41 +93,6 @@ def mars_facts():
 
     # Dataframe to html & quit browser
     return df.to_html()
-
-def hemisphere_data(browser):
-
-    # 1. Use browser to visit the URL 
-    url = 'https://marshemispheres.com'
-    browser.visit(url)
-
-
-    # Parse the resulting html with soup
-    html = browser.html
-    soup2 = soup(html, 'html.parser')
-
-
-    # 2. Create a list to hold the images and titles.
-    hemisphere_image_urls = []
-
-    # 3. Write code to retrieve the image urls and titles for each hemisphere.
-    # for image_url in image_urls:
-
-    # Find and click the full image button
-    i=0
-    for i in range(0,4):
-
-        full_image_elem = browser.find_by_tag('h3')[i]
-        title = full_image_elem.text
-        full_image_elem.click()
-        links_found = browser.links.find_by_text('Sample')[0]
-        link = links_found['href']
-        image_title = {'image_url': link,'title': title}
-        hemisphere_image_urls.append(image_title)
-        i=i+1
-        browser.back()
-
-    # 4. Print the list that holds the dictionary of each image url and title.
-    return hemisphere_image_urls
 
 if __name__ == "__main__":
     
